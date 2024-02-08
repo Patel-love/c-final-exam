2b3e4ef5a5177d97c76a77349a46db6fa7264426
patel-love <manavamrutiya29@gmail.com>
2024-02-08 13:07:59 +0530

    EXAM

---
+++ index.c
@@ -0,0 +1,99 @@
+// --------------------------Q-5----------------------------
+// #include <stdio.h>
+// #include <string.h>
+// struct Laptop{
+//      int price;
+//   char companyname[100];
+//   char processor[100];
+
+// };
+// int main(){
+//  struct Laptop n[10];
+//  int laptopN;
+//  printf("enter laptop number :- ");
+//  scanf("%d",&leptopN);
+
+//  for(int i=0;i<laptopN;i++){
+//     printf("enter price ");
+//     scanf("%d",&n[i].price);
+//     printf("enter company name ");
+//     scanf("%s",&n[i].companyname);
+//     printf("enter processor ");
+//     scanf("%s",&n[i].processor);
+//  }
+
+//  for(int i=0;i<laptopN;i++){
+//     printf("%d\n",n[i].price);
+//     printf("%s\n",n[i].companyname);
+//     printf("%s\n",n[i].processor);
+    
+//  }
+// }
+
+// --------------------------Q-6----------------------------
+// #include <stdio.h>
+// int main(){
+//     FILE *p;
+//     p=fopen("data.txt","w") ;
+//     char data[100] ;
+//     if(p==NULL){
+//         printf("no");
+//     }else{
+//         fputs("red",p);
+//         fclose(p);
+//     }
+// return 0;
+// }
+
+
+// --------------------------Q-4----------------------------
+
+//  #include <stdio.h>
+//  int main(){
+//   int ar[]={1,2,3,4,5};
+
+//   for(int i=0 ;i<=4 ; i++){
+//     if(ar[i]%2==0){
+//         printf("EVEN NUMBER IS %d\n",ar[i]);
+//     }
+//   } 
+//    return 0 ;
+//  }
+
+
+// --------------------------Q-1----------------------------
+//  #include <stdio.h>
+// int main(){
+
+//     int a=100 ;
+//     int b=100 ;
+//     int c=80 ;
+//     int d=70 ;
+//     int e=60 ;
+//   int sum=a+b+c+d+e ;
+//   int ave=sum/5 ;
+//   printf("%d\n",ave) ;
+//   if(ave>90){
+//     printf("A GRADE");
+//   }else if(ave>80){
+//     printf("b GRADE");
+//   }else if(ave>70){
+//     printf("C GRADE");
+//   }else if(ave>60){
+//     printf("D GRADE");
+//   }
+
+//     return 0;
+// }
+
+//   #include <stdio.h>
+//   int fun(int a){
+//     int sum = 0 ;
+//     int ave=0 ;
+
+//     for()
+//   }
+//  int main(){
+
+//     return 0;
+//  }
